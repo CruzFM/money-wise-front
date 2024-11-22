6c3fbf8bc3a23316499e32f8b06cefa654ae7024
Fer Cruz <cruzfernandomartin94@gmail.com>
2024-11-22 19:27:09 -0300

    Login created, functionality done. To do: protect routes, styling.

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,3 +1,77 @@
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
+
+//Primero deberiamos hacer el formulario, con sus validaciones
+    //email
+    //password
+    //submit button
+//Segundo, enviar la informacion al servidor para obtener token
+//Tercero, guardar token en sessionStorage
+//Cuarto, redirigir hacia '/'
+//Quinto, proteger rutas
+    //proteger ruta '/'
+    //proteger rutas /login y /register despues de hecho el /login
+
 export default function Login() {
-    return(<div>Login</div>)
+  const apiUrl = import.meta.env.VITE_API_URL;
+
+  const navigate = useNavigate();
+
+  const LoginSchema = Yup.object().shape({
+    email: Yup.string().email("Invalid email").required("Email required"),
+    password: Yup.string().required("Password required"),
+  });
+
+  const handleLogin = async (value) => {
+    try {
+      const response = await axios.post(`${apiUrl}auth/login`, value);
+      sessionStorage.setItem('auth_token', response.data.token)
+      console.log("Bien ahi wachin, lo lograste!", response.data.token);
+      navigate('/');
+    } catch (error) {
+      console.error("La cagaste en el login hermano: ", error);
+    }
+  };
+
+  const LoginForm = () => (
+    <Formik
+      initialValues={{ email: "", password: "" }}
+      validationSchema={LoginSchema}
+      onSubmit={(values) => {
+        handleLogin(values);
+      }}
+    >
+      {({ isSubmitting }) => (
+        <Form>
+          <div>
+            <Label htmlFor="email">Email</Label>
+            <Field as={Input} type="email" name="email" placeholder="Email" />
+            <ErrorMessage name="email" component="div" />
+          </div>
+          <div>
+            <Label htmlFor="password">Password</Label>
+            <Field
+              as={Input}
+              type="password"
+              name="password"
+              placeholder="Password"
+            />
+            <ErrorMessage name="password" component="div" />
+          </div>
+          <Button type="submit" disabled={isSubmitting}>
+            Submit
+          </Button>
+        </Form>
+      )}
+    </Formik>
+  );
+
+  return(
+    <LoginForm />
+  );
 }
